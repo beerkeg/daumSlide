d0ece3c6199de74fe485f29a3770787641d2ebac
Seong-Rak Choi <ragiragi@hanmail.net>
2012-02-29 15:25:18 +0900

    add fixture

--- src/main/webapp/test-js/slide_spec.js
+++ src/main/webapp/test-js/slide_spec.js
@@ -1,6 +1,21 @@
-describe("greetings", function() {
-    it("say hi", function() {
-        expect("hi").toBeTruthy();
+describe('slide.js', function () {
+    var WRAPPER_ID = 'wrapper_fixture',
+        wrapper;
+
+    beforeEach(function () {
+        wrapper = document.createElement('div');
+        wrapper.id = WRAPPER_ID;
+        document.body.appendChild('wrapper');
+    });
+
+    afterEach(function () {
+        document.body.removeChild(wrapper);
+        wrapper = null;
+    });
+
+    describe('initialize', function () {
+//        var slide = new Slide(document.getElementById(WRAPPER_ID), 0);
+//        expect(slide).toBeDefined();
     });
 
 });
