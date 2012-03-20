aef6d45f10c986155d1e2b5040a44a204d6548ff
eastkiki <eastkiki@hanmail.net>
2012-03-20 20:38:50 +0900

    gesture lib update

--- src/main/webapp/js/lib/gesture.js
+++ src/main/webapp/js/lib/gesture.js
@@ -1,22 +1,22 @@
-(function(c){var b=200;function f(j,i,h){this.type="unindentified";this.direction="unindentified";this.startPos=a(j,i);
-this.delta=a(0,0);this.targetEvent=null;this.term=0;this.startTime=new Date();if(typeof h==="number"){this.gestureThreshold=h
-}else{this.gestureThreshold=30}}f.prototype={};f.prototype.isSwipe=function(){return this.type==="swipe"
-};f.prototype.isScroll=function(){return this.type==="scroll"};f.prototype.isLeft=function(){return this.direction==="left"
-};f.prototype.isRight=function(){return this.direction==="right"};f.prototype.isUp=function(){return this.direction==="up"
-};f.prototype.isDown=function(){return this.direction==="down"};f.prototype.isFlick=function(){return this.term<200?true:false
-};f.prototype.getTerm=function(){return this.term};f.prototype.identifyWithCurrentPos=function(h,k){var i=a(h,k);
-var j=this.delta=i.minus(this.startPos);if(this.type==="unindentified"){if(Math.abs(j.x)>this.gestureThreshold&&Math.abs(j.x)>=Math.abs(j.y)){this.type="swipe";
-if(j.x<0){this.direction="left"}else{this.direction="right"}}else{if(Math.abs(j.y)>this.gestureThreshold&&Math.abs(j.y)>Math.abs(j.x)){this.type="scroll";
-if(j.y<0){this.direction="up"}else{this.direction="down"}}}}};f.prototype.checkFlick=function(){this.term=new Date-this.startTime
-};function a(h,i){return{x:h,y:i,minus:function(j){return a(this.x-j.x,this.y-j.y)}}}function d(h){return(document.addEventListener)?h:"on"+h
+(function(b){var h=30,c=30;function f(k,j,i,l){this.type="unindentified";this.direction="unindentified";
+this.startPos=a(k,j);this.delta=a(0,0);this.targetEvent=null;this.term=0;this.startTime=new Date();this.gestureThreshold=i;
+this.flickThreshold=l}f.prototype={};f.prototype.isSwipe=function(){return this.type==="swipe"};f.prototype.isScroll=function(){return this.type==="scroll"
+};f.prototype.isLeft=function(){return this.direction==="left"};f.prototype.isRight=function(){return this.direction==="right"
+};f.prototype.isUp=function(){return this.direction==="up"};f.prototype.isDown=function(){return this.direction==="down"
+};f.prototype.isFlick=function(){return this.term<200};f.prototype.getTerm=function(){return this.term
+};f.prototype.recognizeGesture=function(i,l){var j=a(i,l);var k=this.delta=j.minus(this.startPos);if(this.type==="unindentified"){if(Math.abs(k.x)>this.gestureThreshold&&Math.abs(k.x)>=Math.abs(k.y)){this.type="swipe";
+if(k.x<0){this.direction="left"}else{this.direction="right"}}else{if(Math.abs(k.y)>this.gestureThreshold&&Math.abs(k.y)>Math.abs(k.x)){this.type="scroll";
+if(k.y<0){this.direction="up"}else{this.direction="down"}}}}};f.prototype.checkFlick=function(){this.term=new Date-this.startTime
+};function a(i,j){return{x:i,y:j,minus:function(k){return a(this.x-k.x,this.y-k.y)}}}function d(i){return(document.addEventListener)?i:"on"+i
 }var g={START:function(){return("ontouchstart" in window)?"touchstart":d("mousedown")}(),MOVE:function(){return("ontouchstart" in window)?"touchmove":d("mousemove")
-}(),END:function(){return("ontouchstart" in window)?"touchend":d("mouseup")}(),listen:function(){if(document.addEventListener){return function(j,i,h){j.addEventListener(i,function(k){h.call(j,k)
-},false)}}else{return function(j,i,h){j.attachEvent(i,function(){h.call(j,window.event)})}}}(),getX:function(i){var h=i.touches?i.touches[0]:i;
-return h.pageX||h.clientX},getY:function(i){var h=i.touches?i.touches[0]:i;return h.pageY||h.clientY}};
-function e(j,h,l){var k=null;if(h){if(typeof h!=="number"){throw new Error("gestureThreshold must be a number")
-}}if(l){if(typeof l==="number"){b=l}else{throw new Error("flickThreshold must be a number")}}var i={onGestureStart:null,onGestureMove:null,onGetstureEnd:null};
-g.listen(j,g.START,function(m){k=new f(g.getX(m),g.getY(m),h);k.targetEvent=m;if(i.onGestureStart){i.onGestureStart(k)
-}});g.listen(j,g.MOVE,function(m){if(k){k.identifyWithCurrentPos(g.getX(m),g.getY(m));k.targetEvent=m;
-if(i.onGestureMove){i.onGestureMove(k)}}});g.listen(j,g.END,function(m){if(k){k.checkFlick();k.targetEvent=m;
-if(i.onGestureEnd){i.onGestureEnd(k)}k=null}});return{onGestureStart:function(m){i.onGestureStart=m},onGestureMove:function(m){i.onGestureMove=m
-},onGestureEnd:function(m){i.onGestureEnd=m}}}c.GestureListener=e;c.GestureSession=f;c.EventUtil=g})(window.gesture={});
+}(),END:function(){return("ontouchstart" in window)?"touchend":d("mouseup")}(),listen:function(){if(document.addEventListener){return function(k,j,i){k.addEventListener(j,function(l){i.call(k,l)
+},false)}}else{return function(k,j,i){k.attachEvent(j,function(){i.call(k,window.event)})}}}(),getX:function(j){var i=j.touches?j.touches[0]:j;
+return i.pageX||i.clientX},getY:function(j){var i=j.touches?j.touches[0]:j;return i.pageY||i.clientY}};
+function e(k,i,n){var l=null,j={onGestureStart:null,onGestureMove:null,onGetstureEnd:null};if(typeof i==="undefined"){i=c
+}else{if(typeof i!=="number"){throw new Error("gestureThreshold must be a number")}}if(typeof n==="undefined"){n=h
+}else{if(typeof n!=="number"){throw new Error("flickThreshold must be a number")}}g.listen(k,g.START,function(o){l=new f(g.getX(o),g.getY(o),i,n);
+l.targetEvent=o;if(j.onGestureStart){j.onGestureStart(l)}});g.listen(k,g.MOVE,function(o){if(l){l.recognizeGesture(g.getX(o),g.getY(o));
+l.targetEvent=o;if(j.onGestureMove){j.onGestureMove(l)}}});function m(o){if(l){l.checkFlick();l.targetEvent=o;
+if(j.onGestureEnd){j.onGestureEnd(l)}l=null}}g.listen(k,g.END,m);if("ontouchstart" in window){g.listen(k,"touchcancel",m)
+}return{onGestureStart:function(o){j.onGestureStart=o},onGestureMove:function(o){j.onGestureMove=o},onGestureEnd:function(o){j.onGestureEnd=o
+}}}b.GestureListener=e;b.GestureSession=f;b.EventUtil=g})(window.gesture={});
