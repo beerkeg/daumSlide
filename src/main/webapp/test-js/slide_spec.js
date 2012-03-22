374fc839079c187bb628b3e73a0d42a43d7ef810
Seong-Rak Choi <ragiragi@hanmail.net>
2012-03-23 00:04:59 +0900

    re-implement datasource

--- src/main/webapp/test-js/slide_spec.js
+++ src/main/webapp/test-js/slide_spec.js
@@ -1,5 +1,6 @@
 /*global describe: false, it: false, xit: false, expect: false,
-  beforeEach: false, afterEach: false, jQuery: false, slide: false */
+  beforeEach: false, afterEach: false, jQuery: false, slide: false,
+  waitsFor: false, runs: false, spyOn: false, jasmine: false */
 describe('slide.js', function () {
     "use strict";
 
@@ -94,8 +95,224 @@ describe('slide.js', function () {
         });
     });
 
-    describe('slide.DataSource', function () {
 
+    describe('slide.DataSource', function () {
+        if (typeof Object.create === 'undefined') {
+            Object.create = function (o) {
+                function F() {}
+                F.prototype = o;
+                return new F();
+            };
+        }
+        function extend(obj, props) {
+            var prop;
+            for (prop in props) {
+                if (props.hasOwnProperty(prop)) {
+                    obj[prop] = props[prop];
+                }
+            }
+        }
+
+        var DataSource = {
+            init: function (data) {
+                var obj = Object.create(this);
+                obj.data = data;
+                obj.index = 0;
+                return obj;
+            },
+            getCurrentSet: function (callback) {
+                var self = this;
+                this.queryPrev(function (prev) {
+                    self.queryNext(function (next) {
+                        callback( {
+                            prev: prev,
+                            current: self.getCurrent(),
+                            next: next
+                        });
+                    });
+                });
+            },
+                queryPrev: function (callback) {
+                    if (this.index - 1 < 0) { // reaches at first
+                        this.willQueryFirstOfData(function (prev) {
+                            callback(prev);
+                        });
+                    } else {
+                        callback(this.data[this.index - 1]);
+                    }
+                },
+                getCurrent: function () {
+                    return this.data[this.index];
+                },
+                queryNext: function (callback) {
+                    if (this.index + 1 >= this.data.length) { // reaches end
+                        this.willQueryEndOfData(function (next) {
+                            callback(next);
+                        });
+                    } else {
+                        callback(this.data[this.index + 1]);
+                    }
+                },
+            next: function () {
+                this.index += 1;
+            },
+            prev: function () {
+                this.index -= 1;
+            },
+            willQueryEndOfData: function (callback) {
+                callback(null);
+            },
+            willQueryFirstOfData: function (callback) {
+                callback(null);
+            },
+            concatData: function (addends) {
+                this.data = this.data.concat(addends);
+            }
+        };
+
+        it('should serve current data set', function () {
+            var ds = DataSource.init([1, 2, 3, 4, 5]),
+                currentSet;
+            ds.getCurrentSet(function (set) {
+                currentSet = set;
+            });
+            waitsFor(function () {
+                return currentSet;
+            });
+            runs(function () {
+                expect(currentSet).toEqual({
+                    prev: null,
+                    current: 1,
+                    next: 2
+                });
+            });
+        });
+
+        it('should call proper handler when reaching end of data', function () {
+            var ds = DataSource.init([1, 2, 3, 4, 5]),
+                currentSet;
+
+            ds.index = 4;
+
+            ds.willQueryEndOfData = function (callback) {
+                var newData = [6, 7, 8];
+                ds.concatData(newData);
+                callback(newData[0]);
+            };
+            ds.getCurrentSet(function (set) {
+                currentSet = set;
+            });
+
+            waitsFor(function () {
+                return currentSet;
+            });
+            runs(function () {
+                expect(currentSet).toEqual({
+                    prev: 4,
+                    current: 5,
+                    next: 6
+                });
+            });
+        });
+
+        it('should call proper handler when reaching first of data', function () {
+            var ds = DataSource.init([1, 2, 3, 4, 5]),
+                currentSet;
+
+            ds.index = 0;
+
+            ds.willQueryFirstOfData = function (callback) {
+                var data = ds.data;
+                callback(data[data.length - 1]);
+            };
+            ds.getCurrentSet(function (set) {
+                currentSet = set;
+            });
+
+            waitsFor(function () {
+                return currentSet;
+            });
+            runs(function () {
+                expect(currentSet).toEqual({
+                    prev: 5,
+                    current: 1,
+                    next: 2
+                });
+            });
+        });
+
+        describe('infinite datasource', function () {
+
+            var InfiniteDataSource = Object.create(DataSource);
+            extend(InfiniteDataSource, {
+                next: function () {
+                    if (this.index + 1 >= this.data.length) {
+                        this.index = 0;
+                    } else {
+                        this.index += 1;
+                    }
+                },
+                prev: function () {
+                    if (this.index <= 0) {
+                        this.index = this.data.length - 1;
+                    } else {
+                        this.index -= 1;
+                    }
+                },
+                willQueryFirstOfData: function (callback) {
+                    var data = this.data;
+                    callback(data[data.length - 1]);
+                },
+                willQueryEndOfData: function (callback) {
+                    var data = this.data;
+                    callback(data[0]);
+                }
+            });
+
+            it('should loop infinitly', function () {
+                var ids = InfiniteDataSource.init([1, 2, 3]),
+                    currentSet;
+                function queryCurrentSet() {
+                    currentSet = null;
+                    ids.getCurrentSet(function (set) {
+                        currentSet = set;
+                    });
+                }
+
+                runs(function () {
+                    queryCurrentSet();
+                });
+                waitsFor(function () {
+                    return currentSet;
+                });
+                runs(function () {
+                    expect(currentSet).toEqual({ prev: 3, current: 1, next: 2 });
+                });
+
+                runs(function () {
+                    ids.next();
+                    queryCurrentSet();
+                });
+                waitsFor(function () {
+                    return currentSet;
+                });
+                runs(function () {
+                    expect(currentSet).toEqual({ prev: 1, current: 2, next: 3 });
+                });
+
+                runs(function () {
+                    ids.next();
+                    queryCurrentSet();
+                });
+                waitsFor(function () {
+                    return currentSet;
+                });
+                runs(function () {
+                    expect(currentSet).toEqual({ prev: 2, current: 3, next: 1 });
+                });
+            });
+        });
     });
 
 });
+
