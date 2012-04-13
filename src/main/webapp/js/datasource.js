8182f8b86da59c2a7aa31e11e154c741d9f6109e
eastkiki <eastkiki@hanmail.net>
2012-04-13 11:27:33 +0900

    datasource.js 주석 추가

--- src/main/webapp/js/datasource.js
+++ src/main/webapp/js/datasource.js
@@ -8,19 +8,23 @@
     var DataSource = exports.DataSource = Class.extend({
         /**
          * 새로운 DataSource를 생성/초기화한다.
-         * @param data
+         * @param data {Array}
          */
         init: function (data) {
             this.data = data;
             this.index = 0;
         },
+        /**
+         * 현재 인덱스를 설정한다.
+         * @param index {Integer}
+         */
         setCurrentIndex: function (index) {
             this.index = index;
         },
         /**
          * 현재/이전/다음 데이터셋을 불러온다.
          * 데이터가 없을 경우 해당 필드는 null 로 세팅된다.
-         * @param callback
+         * @param callback {Function}
          */
         queryCurrentSet: function (callback) {
             var self = this;
@@ -36,6 +40,11 @@
                 });
             });
         },
+        /**
+         * 이전 데이터를 불러온다.
+         * 데이터가 없을 경우 해당 필드는 null 로 세팅된다.
+         * @param callback {Function}
+         */
         queryPrev: function (callback) {
             if (this.index - 1 < 0) { // reaches at first
                 if (typeof this.willQueryFirstOfDataDelegate === 'function') {
@@ -49,9 +58,18 @@
                 callback(this.data[this.index - 1]);
             }
         },
+        /**
+         * 현재 데이터를 불러온다.
+         * @param callback {Function}
+         */
         queryCurrent: function (callback) {
             callback(this.data[this.index]);
         },
+        /**
+         * 다음 데이터를 불러온다.
+         * 데이터가 없을 경우 해당 필드는 null 로 세팅된다.
+         * @param callback {Function}
+         */
         queryNext: function (callback) {
             if (this.index + 1 >= this.data.length) { // reaches end
                 if (typeof this.willQueryEndOfDataDelegate === 'function') {
@@ -79,7 +97,7 @@
         },
         /**
          * 데이터 끝에 도달하였을 때 호출될 delegate를 설정한다.
-         * @param delegate
+         * @param delegate {Function}
          */
         willQueryEndOfData: function (delegate) {
             this.willQueryEndOfDataDelegate = delegate;
@@ -87,14 +105,14 @@
         /**
          * 현재 데이터 끝에 도달하였을 때 호출될 기본 delegate.
          * callback에 그 다음 데이터를 넘겨 호출하여 준다.
-         * @param callback
+         * @param callback {Function}
          */
         willQueryEndOfDataDelegate: function (callback) {
             callback(null);
         },
         /**
          * 데이터 시작에 도달하였을 때 호출될 delegate를 설정한다.
-         * @param delegate
+         * @param delegate {Function}
          */
         willQueryFirstOfData: function (delegate) {
             this.willQueryFirstOfDataDelegate = delegate;
@@ -132,6 +150,9 @@
         init: function (data) {
             this._super(data);
         },
+        /**
+         * 다음 데이터로 이동, 다음 데이터가 없을 경우 맨 처음 데이터로 이동.
+         */
         next: function () {
             if (this.index + 1 >= this.data.length) {
                 this.index = 0;
@@ -139,6 +160,9 @@
                 this.index += 1;
             }
         },
+        /**
+         * 이전 데이터로 이동, 이전 데이터가 없을 경우 맨 끝 데이터로 이동.
+         */
         prev: function () {
             if (this.index <= 0) {
                 this.index = this.data.length - 1;
