1fe24bf96f7859168896e2f4513954bd003c9b34
eastkiki <eastkiki@hanmail.net>
2012-04-18 18:55:59 +0900

    remote 파일도 build에 추가되도록 수정.

--- build/build.js
+++ build/build.js
@@ -2,17 +2,20 @@
 (function () {
     'use strict';
 
-    var async = require('async'),
+    var http = require('http'),
+        async = require('async'),
         clc = require('cli-color'),
         fs = require('fs'),
-        jsp = require("uglify-js").parser,
-		pro = require("uglify-js").uglify;
+        jsp = require('uglify-js').parser,
+		pro = require('uglify-js').uglify;
 
     function error() {
         console.error(clc.red(Array.prototype.join.call(arguments, ' ')));
     }
 
     var fileList = [
+                'http://m1.daumcdn.net/svc/attach/U0301/cssjs/gesture/gesture-1.0.0.min.js',
+                'http://m1.daumcdn.net/svc/attach/U0301/cssjs/ejohn/class-0.1.0.min.js',
                 'datasource.js',
                 'util.js',
                 'panel.js',
@@ -23,15 +26,49 @@
 
     var upload = exports.upload = function (fileList, callback) {
 
+        function checkfiles(files) {
+            for(var i = 0, len = files.length; i<len; i++) {
+                if (!files[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
         // STEP #1. read file from fileList
         function readFiles(cb) {
             var files = [];
-            fileList.forEach(function (filename) {
-                var filepath = __dirname + "/../src/main/webapp/js/" + filename;
-                files.push(fs.readFileSync(filepath, "utf-8"));
+            fileList.forEach(function (filename, index) {
+                if (filename.indexOf("http://") > -1) {
+                    var value = filename.replace("http://",""),
+                        i = value.indexOf("/");
+
+                    var fileReq = http.request({
+                        host: value.substring(0, i),
+                        port: 80,
+                        path: value.substring(i),
+                        method: 'GET'
+                    }, function (res) {
+                        res.setEncoding('utf8');
+                        res.on('data', function (chunk) {
+                            files[index] = chunk;
+                            if (checkfiles(files)) {
+                                cb(null, files);
+                            }
+                        });
+                    });
+                    fileReq.on('error', function (e) {
+                        error('problem with uploadUrl request: ' + e.message);
+                        cb(e);
+                    });
+                    fileReq.end();
+                } else {
+                    var filepath = __dirname + "/../src/main/webapp/js/" + filename;
+                    files[index] = fs.readFileSync(filepath, "utf-8");
+                    if (checkfiles(files)) {
+                        cb(null, files);
+                    }
+                }
             });
-            console.log(files);
-            cb(null, files);
         }
         
         // STEP #2. merge files
