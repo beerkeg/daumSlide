592fed73034e666e1af9ab6777c4989828648634
eastkiki <eastkiki@hanmail.net>
2012-04-20 16:25:04 +0900

    build script 수정.

--- build/build.js
+++ build/build.js
@@ -6,16 +6,15 @@
         async = require('async'),
         clc = require('cli-color'),
         fs = require('fs'),
-        jsp = require('uglify-js').parser,
-		pro = require('uglify-js').uglify;
+        compressor = require('node-minify');
 
     function error() {
         console.error(clc.red(Array.prototype.join.call(arguments, ' ')));
     }
 
-    var fileList = [
-                'http://m1.daumcdn.net/svc/attach/U0301/cssjs/gesture/gesture-1.0.0.min.js',
+    var fileList = exports.fileList = [
                 'http://m1.daumcdn.net/svc/attach/U0301/cssjs/ejohn/class-0.1.0.min.js',
+                'http://m1.daumcdn.net/svc/attach/U0301/cssjs/gesture/gesture-1.0.0.js',
                 'datasource.js',
                 'util.js',
                 'panel.js',
@@ -24,9 +23,10 @@
                 'slide.js'
             ];
 
-    var upload = exports.upload = function (fileList, callback) {
+    var build = exports.upload = function (destFileName, fileList, callback) {
+        var files = [];
 
-        function checkfiles(files) {
+        function isReadAllfiles() {
             for(var i = 0, len = files.length; i<len; i++) {
                 if (!files[i]) {
                     return false;
@@ -34,79 +34,128 @@
             }
             return true;
         }
-        // STEP #1. read file from fileList
-        function readFiles(cb) {
-            var files = [];
-            fileList.forEach(function (filename, index) {
-                if (filename.indexOf("http://") > -1) {
-                    var value = filename.replace("http://",""),
-                        i = value.indexOf("/");
 
-                    var fileReq = http.request({
-                        host: value.substring(0, i),
-                        port: 80,
-                        path: value.substring(i),
-                        method: 'GET'
-                    }, function (res) {
-                        res.setEncoding('utf8');
-                        res.on('data', function (chunk) {
-                            files[index] = chunk;
-                            if (checkfiles(files)) {
-                                cb(null, files);
-                            }
-                        });
-                    });
-                    fileReq.on('error', function (e) {
-                        error('problem with uploadUrl request: ' + e.message);
-                        cb(e);
-                    });
-                    fileReq.end();
+        function isRemoteFile(filename) {
+            return (/(https?):\/\/([a-z0-9-]+\.)+[a-z0-9]{2,4}.*$/).test(filename);
+        }
+
+        function readRemoteFile(filename, index, cb) {
+            var value = filename.replace(/(https?):\/\//,""),
+                i = value.indexOf("/");
+
+            var fileReq = http.request({
+                host: value.substring(0, i),
+                port: 80,
+                path: value.substring(i),
+                method: 'GET'
+            }, function (res) {
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    if (files[index]) {
+                        files[index] += data;
+                    } else {
+                        files[index] = data;
+                    }
+                });
+                res.on('end', function () {
+                    if (isReadAllfiles()) {
+                        cb(null);
+                    }
+                });
+                res.on('error', function (err) {
+                    cb(err);
+                });
+            });
+            fileReq.on('error', function (err) {
+                error('problem with uploadUrl request: ' + err.message);
+                cb(err);
+            });
+            fileReq.end();
+        }
+        function readLocalFile(filename, index, cb) {
+            var filepath = __dirname + "/../src/main/webapp/js/" + filename;
+            fs.readFile(filepath, "utf-8", function (err, data) {
+                if (err) {
+                    cb(err);
                 } else {
-                    var filepath = __dirname + "/../src/main/webapp/js/" + filename;
-                    files[index] = fs.readFileSync(filepath, "utf-8");
-                    if (checkfiles(files)) {
-                        cb(null, files);
+                    files[index] = data;
+                    if (isReadAllfiles()) {
+                        cb(null);
                     }
                 }
             });
         }
+
+        // STEP #1. read file from fileList
+        function readFile(cb) {
+            var callFile = [];
+            fileList.forEach(function (filename, index) {
+                if (isRemoteFile(filename)) {
+                    readRemoteFile(filename, index, cb);
+                } else {
+                    readLocalFile(filename, index, cb);
+                }
+            });
+        }
         
-        // STEP #2. merge files
-        function mergefiles(files, cb) {
-            var filepath = __dirname + "/slide.merged.js";
-            fs.writeFile(filepath, files.join(""), "utf-8", function (err) {
+        // STEP #2. merge files to "slide.merged.js"
+        function mergefiles(cb) {
+            var filepath = __dirname + "/"+destFileName+".merged.js";
+            fs.writeFile(filepath, files.join("\n\n\n"), "utf-8", function (err) {
                 if (err) {
                     cb(err);
-                    return ;
-                } 
-                var file = fs.readFileSync(filepath, "utf-8");
-                cb(null, file);
+                } else {
+                    fs.readFile(filepath, "utf-8", function (err, data) {
+                        if (err) {
+                            cb(err);
+                        } else {
+                            cb(null, data);
+                        }
+                    });
+                }
             });
         }
 
-        // STEP #3 minify "slide.merged.js"
-        function minifyFile(file, cb) {
-            var ast = jsp.parse(file); // parse code and get the initial AST
-            ast = pro.ast_mangle(ast); // get a new AST with mangled names
-            ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
-            var final_code = pro.gen_code(ast); // compressed code here
-            cb(final_code);
+        // STEP #3 minify "slide.merged.js" to "slide.min.js"
+        function minifyFile(data, cb) {
+            // try {
+            //     new compressor.minify({
+            //         type: 'yui',
+            //         fileIn: __dirname + "/"+destFileName+".merged.js",
+            //         fileOut: __dirname + "/"+destFileName+".min.js",
+            //         callback: function(err){
+            //             if (err) {
+            //                 cb(err);
+            //             } else {
+            //                 cb(null);
+            //             }
+            //         }
+            //     });
+            // } catch (err) {
+            //     cb(err);
+            // }
+            var command = "uglifyjs --output " +
+                            __dirname + "/"+destFileName+".min.js " +
+                            __dirname + "/" + destFileName + ".merged.js";
+            console.log(command);
+            require('child_process').exec(command, function (error, stdout, stderr){
+                if (error !== null) {
+                  console.error(error.stack);
+                }
+            });
         }
 
         async.waterfall([
-            readFiles,
+            readFile,
             mergefiles,
             minifyFile
         ], callback);
     };
 
-    upload(fileList, function(data){
-        var filepath = __dirname + "/slide.min.js";
-        fs.writeFile(filepath, data, "utf-8", function (err) {
-            if (err) {
-                error(err);
-                return ;
-            } 
-        });
+    build("slide", fileList, function(err){
+        if (err) {
+            error("error :");
+            console.error(err.stack);
+        }
     });
 })();
