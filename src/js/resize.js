2a8b1c0b2def803e87dcd6dec3b6334e031825d9
kojak17 <kojak17@daumcorp.com>
2013-10-22 17:10:32 +0900

    remove matchmedia for detect resize

--- src/js/resize.js
+++ src/js/resize.js
@@ -7,50 +7,34 @@
 
 	var ua = exports.ua;
     var os = ua.os;
-    var osVersion = os.version;
-    var browser = ua.browser;
-    var browserVersion = browser.version;
+    var availOrientationChange = ("onorientationchange" in window && !os.android && ua.platform !== "pc") ? true : false;
 
-	function isOverAndroidHoneycomb() {
-        return !!(os.android && parseInt(osVersion.major, 10) > 3);
-    }
-    
-    var availMatchMedia = (typeof window.matchMedia === "function" && isOverAndroidHoneycomb()) ? true : false,
-        availOrientationChange = ("onorientationchange" in window && !os.android && ua.platform !== "pc") ? true : false;
-
-    function fixbugOnMatchMedia() {
-        var style = document.createElement("style");
-        style.innerHTML = "@media all and (orientation:portrait){.f{}}@media all and (orientation:landscape){.f{}}";
-        document.getElementsByTagName("head")[0].appendChild(style);
-    }
-
-    exports.checkResize = function(el, callback) {
-        setTimeout(function(){
-            var width = el.clientWidth,
-                height = el.clientHeight;
-            if (width > 0 && height > 0) {
-                callback(width, height);
-            }
-        }, 50);
-    };
     exports.onResized = function () {
-        if (availMatchMedia) {
-            fixbugOnMatchMedia();
-            var mql = window.matchMedia("(orientation: landscape)");
+        var resizeEvent = availOrientationChange ? "orientationchange" : "resize";
+        var width = 0, height = 0;
 
-            return function (el, callback) {
-                mql.addListener(function(){
-                    exports.checkResize(el, callback);
-                });
-            };
-        } else {
-            var resizeEvent = availOrientationChange ? "orientationchange" : "resize";
+        return function (el, callback) {
+            function isSizeReallyChanged () {
+                return !(width === el.clientWidth && height === el.clientHeight);
+            }
 
-            return function (el, callback) {
-                window.gesture.EventUtil.listen(window, resizeEvent, function () {
-                    exports.checkResize(el, callback);
-                });
-            };
-        }
+            window.gesture.EventUtil.listen(window, resizeEvent, function () {
+                var cnt = 0;
+                setTimeout(function _checkResize() {
+                    if (isSizeReallyChanged()) {
+                        width = el.clientWidth;
+                        height = el.clientHeight;
+
+                        if(width > 0 && height > 0) {
+                            callback(width, height);
+                        }
+                    } else {
+                        if (cnt++ < 20) {
+                            setTimeout(_checkResize, 50);
+                        }
+                    }
+                }, 50);
+            });
+        };
     }();
 }(window.slide = (typeof slide === 'undefined') ? {} : slide));
