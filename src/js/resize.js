4014d8b60cccacf2f2e7d8a1c2f53ff608c78d4f
kojak17 <kojak17@daumcorp.com>
2013-11-12 13:55:22 +0900

    이전 디텍팅 방식으로 수정(orientation only)

--- src/js/resize.js
+++ src/js/resize.js
@@ -9,37 +9,40 @@
     var os = ua.os;
     var availOrientationChange = ("onorientationchange" in window && ua.platform !== "pc") ? true : false;
 
+    /**
+     * android: orientation > resize
+     * ios    : resize > orientation 순으로 이벤트가 발생된다.
+     * ios의 경우, resize eventHandler가 많을 경우
+     * orientation event가 지연(200ms 이상)되기 때문에,
+     * orientation 이벤트로만 리사이즈를 체크한다.
+     */
     exports.onResized = function () {
-        var width = 0, height = 0;
+        var prevWidth = 0, prevHeight = 0;
+        var resizeEvent = availOrientationChange ? 'orientationchange' : 'resize';
 
         return function (el, callback) {
-            function isChanged () {
-                return !(width === el.clientWidth && height === el.clientHeight);
+            function isChanged (width, height) {
+                return !(prevWidth === width && prevHeight === height);
             }
 
-            var isOrientationChange = false, isResize = false;
-            function checkResize() {
-                if ((isOrientationChange || !availOrientationChange) &&
-                    isResize && isChanged()) {
-                    width = el.clientWidth;
-                    height = el.clientHeight;
-                    callback(width, height);
+            exports.on(window, resizeEvent, function() {
+                var cnt = 0;
+                function checkResize() {
+                    var width = el.clientWidth;
+                    var height = el.clientHeight;
+
+                    if(isChanged(width, height)) {
+                        prevWidth = width;
+                        prevHeight = height;
+                        callback(width, height);
+
+                    } else if(cnt < 10) {
+                        cnt++;
+                        setTimeout(checkResize, 100);
+                    }
                 }
-            }
 
-            exports.on(window, 'orientationchange', function() {
-                isOrientationChange = true;
                 checkResize();
-            });
-
-            exports.on(window, 'resize', function() {
-                isResize = true;
-                checkResize();
-                window.setTimeout(function() {
-                    checkResize();
-                    isOrientationChange = false;
-                    isResize = false;
-                }, 200);
             });
         };
     }();
