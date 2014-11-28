7b6ebc14a40ebec31ef3f26b8fa6a77adb4d8a86
lyle.k <lyle.k@daumkakao.com>
2014-11-28 14:46:56 +0900

    fix

---
+++ javascripts/introduce.js
@@ -0,0 +1,246 @@
+(function(exports) {
+    'use strict';
+
+    var loader = new exports.SearchImageLoader('고흐');
+    var numbers = [];
+    for(var i=0; i<10; i+=1) {
+        numbers.push({
+            id: i,
+            toHTML: function (panel, slide) {
+                return '<div class="number">' + (this.id + 1) + '</div>';
+            }
+        });
+    }
+
+    function launchDefaultCase(elFrame, elPrev, elNext) {
+        var ds = new slide.DataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds);
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchDividedCase(elFrame, elPrev, elNext) {
+        var ds = new slide.DataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelsToShow: 3
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchFixedCase(elFrame, elPrev, elNext) {
+        var ds = new slide.DataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelType: slide.FIXED,
+            panelWidth: 150
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchDividedCenterCase(elFrame, elPrev, elNext) {
+        var ds = new slide.DataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelsToShow: 2,
+            isCenterAligned: true
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchFixedCenterCase(elFrame, elPrev, elNext) {
+        var ds = new slide.DataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelType: slide.FIXED,
+            panelWidth: 150,
+            isCenterAligned: true
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchInfiniteCase(elFrame, elPrev, elNext) {
+        var ds = new slide.InfiniteDataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelsToShow: 3
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchAppendCase(elFrame, elPrev, elNext) {
+        loader.load(function(builded) {
+            var ExtendedDataSource = slide.DataSource.extend({
+                willQueryEndOfDataDelegate: function (callback, index) {
+                    var self = this;
+                    loader.load(function (builded) {
+                        self.addNextData(builded);
+                        callback(builded[0]);
+                    });
+                }
+            });
+
+            var ds = new ExtendedDataSource(builded);
+            var sl = new slide.Slide(elFrame, ds, {
+                panelType: slide.FIXED,
+                panelWidth: 150
+            });
+            elPrev.addEventListener('click', function() {
+                sl.prev();
+            });
+            elNext.addEventListener('click', function() {
+                sl.next();
+            });
+        });
+    }
+
+    function launchCustomEventCase(elFrame, elPrev, elNext) {
+        var ds = new slide.InfiniteDataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            panelsToShow: 3
+        });
+
+        var timeId = 0;
+        var elText = document.querySelector('.firing_event');
+        function setEventText(event) {
+            window.clearTimeout(timeId);
+            elText.classList.add('fire');
+            elText.innerHTML = event;
+
+            timeId = window.setTimeout(function() {
+                elText.classList.remove('fire');
+                elText.innerHTML = 'idle';
+            }, 500);
+        }
+
+        sl.on('prev:before', function() {
+            setEventText('prev:before');
+        });
+        sl.on('prev', function() {
+            setEventText('prev');
+        });
+        sl.on('next:before', function() {
+            setEventText('next:before');
+        });
+        sl.on('next', function() {
+            setEventText('next');
+        });
+        sl.on('cancel', function() {
+            setEventText('cancel');
+        });
+        sl.on('click', function() {
+            setEventText('click');
+        });
+        sl.on('resize', function() {
+            setEventText('resize');
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    function launchCustomAnimationCase(elFrame, elPrev, elNext) {
+        var ds = new slide.InfiniteDataSource(numbers);
+        var sl = new slide.Slide(elFrame, ds, {
+            delegate: {
+                updateBasePosition: function(movedOffset) {
+                    var position = this.getPositionByOffset(movedOffset);
+                    this.basePosition.deg -= position.deg;
+                },
+                getPositionByOffset: function(offset) {
+                    return {
+                        deg: offset * 90
+                    };
+                },
+                getPositionByGesture: function(deltaX, deltaY) {
+                    var panelWidth = this.controller.panelWidth;
+                    var gestureRatio = this.controller.gestureRatio;
+                    return {
+                        deg: (deltaX*gestureRatio/panelWidth)*90
+                    };
+                },
+                movePanelPosition: function (panelIndex, position) {
+                    var panel = this.container.getPanel(panelIndex);
+                    var _deg = position.deg - this.basePosition.deg;
+                    panel.setTransform('rotateY(' + _deg +
+                    'deg) translate3d(0px, 0px, ' +
+                    this.slide.perspectiveFactor + 'px)');
+                },
+                moveSlidePosition: function (position) {
+                    var _deg = position.deg + this.basePosition.deg;
+                    this.container.setTransform('translate3d(0px, 0px, -' +
+                    this.slide.perspectiveFactor + 'px) rotateY(' +
+                    _deg + 'deg)');
+                },
+                resizePanels: function() {
+                    var frameEl = this.slide.frameEl;
+                    frameEl.style[slide.PERSPECTIVE] = '1000px';
+                    frameEl.style[slide.TRANSFORM_STYLE] = 'preserve-3d';
+                    this.container.setStyle(slide.TRANSFORM_STYLE, 'preserve-3d');
+                    this.slide.perspectiveFactor = this.frameWidth / 2;
+
+                    this.container.setPanelStyle('width', this.panelWidth + 'px');
+                    this.animator.setDefaultSlidePosition();
+                }
+            }
+        });
+
+        elPrev.addEventListener('click', function() {
+            sl.prev();
+        });
+        elNext.addEventListener('click', function() {
+            sl.next();
+        });
+    }
+
+    exports.onload = function initislize() {
+        var elCase = document.querySelectorAll('.wrapper .slide_frame');
+        var elPrev = document.querySelectorAll('.wrapper .prev');
+        var elNext = document.querySelectorAll('.wrapper .next');
+
+
+        launchDefaultCase(elCase[0], elPrev[0], elNext[0]);
+        launchDividedCase(elCase[1], elPrev[1], elNext[1]);
+        launchFixedCase(elCase[2], elPrev[2], elNext[2]);
+        launchDividedCenterCase(elCase[3], elPrev[3], elNext[3]);
+        launchFixedCenterCase(elCase[4], elPrev[4], elNext[4]);
+        launchInfiniteCase(elCase[5], elPrev[5], elNext[5]);
+        launchAppendCase(elCase[6], elPrev[6], elNext[6]);
+        launchCustomEventCase(elCase[7], elPrev[7], elNext[7]);
+        launchCustomAnimationCase(elCase[8], elPrev[8], elNext[8]);
+    }
+}(window));
---
+++ javascripts/search_image_loader.js
@@ -0,0 +1,59 @@
+(function(exports) {
+    exports.SearchImageLoader = Class.extend({
+        init: function(query) {
+            this.pool = document.getElementsByTagName("head")[0];
+            this.query = encodeURIComponent(query);
+            this.pageNum = 0;
+            this.callbackId = 0;
+        },
+        simpleSearchAPIJsonp: function(callback) {
+            var callbackId = '_simpleJsonpCallback' + (this.callbackId += 1);
+            exports[callbackId] = callback;
+
+            var url = 'http://apis.daum.net/search/image?q='+ this.query +
+                '&output=json&callback=' + callbackId + '&pageno=' + this.pageNum +
+                '&apikey=0bdf91fedab0c293962cd90855358777e15944d8';
+            var s = document.createElement("script");
+            s.type = "text/javascript";
+            s.src = url;
+            s.charset = "utf-8";
+            this.pool.appendChild(s);
+        },
+        load: function (callback) {
+            var self = this;
+            this.pageNum += 1;
+            this.simpleSearchAPIJsonp(function(data) {
+                var datasource = self.build(data);
+                callback(datasource);
+            });
+        },
+        build: function(data) {
+            var items = data.channel.item, arr = [];
+            for (var i = 0, len = items.length; i < len; i++) {
+                var item = items[i];
+                arr.push({
+                    thumbnail: item.thumbnail,
+                    width: item.width,
+                    height: item.height,
+                    title: item.title,
+                    link: item.link,
+                    toHTML: function (panel, slide) {
+                        var img = new Image();
+                        img.className = 'image';
+                        img.src = this.thumbnail;
+                        img.onload = function() {
+                            panel.clear();
+                            panel.el.appendChild(img);
+                        };
+                        img.ondragstart = function(e) {
+                            daumtools.event.preventDefault(e);
+                        };
+
+                        return '<div class="spinner"></div>';
+                    }
+                });
+            }
+            return arr;
+        }
+    });
+}(window));
